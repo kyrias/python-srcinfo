16f08b82472188cfe984afb15b5fa79636a46e14
Johannes Löthberg <johannes@kyriasis.com>
2016-02-06 19:21:26 +0100

    Release 0.0.3
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = 'srcinfo',
-    version = '0.0.2',
+    version = '0.0.3',
     packages = ['srcinfo'],
 
     description = ('A small library to parse .SRCINFO files'),
