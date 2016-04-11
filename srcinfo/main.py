1043741afe72fe273b7e6c9b09e664bd4501d26e
Matthew Gamble <git@matthewgamble.net>
2016-04-11 21:10:19 +1000

    Add support for JSON output

--- srcinfo/main.py
+++ srcinfo/main.py
@@ -5,14 +5,23 @@ import sys
 # Parse SRCINFO from stdin and pretty-print parsed info
 #
 def main():
-    import pprint
     import sys
+
+    output_json = len(sys.argv) > 1 and sys.argv[1] == "--json"
 
     srcinfo = sys.stdin.read()
     (info, errors) = parse_srcinfo(srcinfo)
 
-    pprint.PrettyPrinter().pprint(info)
-    if errors:
-        print('==> The following errors occured while parsing:', file=sys.stderr)
-        for error in errors:
-            print(error, file=sys.stderr)
+    if output_json:
+        if errors:
+            sys.exit(1)
+        else:
+            import json
+            print(json.dumps(info))
+    else:
+        import pprint
+        pprint.PrettyPrinter().pprint(info)
+        if errors:
+            print('==> The following errors occured while parsing:', file=sys.stderr)
+            for error in errors:
+                print(error, file=sys.stderr)
